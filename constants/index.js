a28f4623770afcae765064d1e39e1067f07f0703
Sathsara Hewage <sathsarahewage@gmail.com>
2026-02-17 00:21:50 +0530

    feat: implement NavBar component and update App structure

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { id: "cocktails", title: "Cocktails" },
+  { id: "about", title: "About Us" },
+  { id: "menu", title: "Menu" },
+  { id: "events", title: "Events" },
+  { id: "contact", title: "Contact Us" },
+];
