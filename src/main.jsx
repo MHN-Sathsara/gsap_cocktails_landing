15236f4abbcabf3435b0cf2f07609aacf63e9909
Sathsara Hewage <sathsarahewage@gmail.com>
2026-02-24 17:43:01 +0530

    feat: add Contact component with GSAP animations and integrate into App and few other small fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { gsap } from "gsap";
+import { CSSPlugin } from "gsap/CSSPlugin";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Register the plugin
+gsap.registerPlugin(CSSPlugin);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
